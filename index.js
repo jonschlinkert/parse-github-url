bd5041eddaee3ea706d4ca393b83d2238135eda8
jonschlinkert <jon.schlinkert@sellside.com>
2017-10-31 20:18:30 -0400

    upgrade devDependencies, run `update`

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /*!
  * parse-github-url <https://github.com/jonschlinkert/parse-github-url>
  *
- * Copyright (c) 2015, Jon Schlinkert.
- * Licensed under the MIT License.
+ * Copyright (c) 2015-2017, Jon Schlinkert.
+ * Released under the MIT License.
  */
 
 'use strict';
